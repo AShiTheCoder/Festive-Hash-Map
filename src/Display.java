f4293228b18de8721c708ed2278868ea64e4b2c8
Andrew Shi <ashilol95@gmail.com>
2016-01-09 12:46:26 -0800

    Added some more features
    Includes auto-scaled y axis, user chooses number of dice, sides, and
    rolls

--- src/Display.java
+++ src/Display.java
@@ -5,23 +5,32 @@ import java.util.*;
 import javax.swing.*;
 
 public class Display extends JFrame implements MouseListener{
-	static final int ROLLS = 1000;
-	static final int CAPACITY = (int) (Math.ceil(ROLLS*1.3));
+	private int rolls;
+	private int capacity = (int) (Math.ceil(rolls*1.3));
 	private double y_scale;
-	static HashMap<String,Integer> h = new HashMap<String,Integer>(CAPACITY);
-	static final int SIDES = 6;
-	static final int POS = SIDES*4-3;
+	private HashMap<String,Integer> h = new HashMap<String,Integer>(capacity);
+	private int sides;
+	private int pos;
 	private boolean erase = false;
+	private Die[] rollers;
+	private int dice;
+	private int max = 0;
 	
-	public Display(){
+	public Display(int r, int s, Die[] d){
+		rolls = r;
+		sides = s;
+		rollers = d;
+		dice = rollers.length;
+		pos = (sides-1)*dice+1;
 		init();
 	}
 	
 	public void init(){
 		setSize(1040,600);
 		addMouseListener(this);
-		initRolls();
-		y_scale = (this.getHeight()-200.0)/200;
+		initrolls();
+		findMax();
+		y_scale = (this.getHeight()-200.0)/max;
 	}
 	
 	public void paint(Graphics g) {
@@ -39,18 +48,18 @@ public class Display extends JFrame implements MouseListener{
 		g.drawLine(100, 100, 100, this.getHeight()-100); //draws Y axis
 		g.drawLine(100, this.getHeight()-100, this.getWidth()-100, this.getHeight()-100); //draws X axis
 		
-		for (int i = 0; i < 5; i++){	//draws Y axis markings
-			g.drawLine(95, ((this.getHeight()-200)/4)*i+100, 100, ((this.getHeight()-200)/4)*i+100);
+		for (int i = 0; i < (max/50)+1; i++){	//draws Y axis markings
+			g.drawLine(95, ((this.getHeight()-200)/(max/50))*i+100, 100, ((this.getHeight()-200)/(max/50))*i+100);
 		}
-		for (int i = 0; i < POS+1; i++){	//draws X axis markings
-			g.drawLine(((this.getWidth()-200)/POS)*i+100, this.getHeight()-100, ((this.getWidth()-200)/POS)*i+100, this.getHeight()-95);
+		for (int i = 0; i < pos+1; i++){	//draws X axis markings
+			g.drawLine(((this.getWidth()-200)/pos)*i+100, this.getHeight()-100, ((this.getWidth()-200)/pos)*i+100, this.getHeight()-95);
 		}
 		
-		for (int i = 0; i < POS; i++){	//draws X axis numbers
-			g.drawString(String.valueOf(i+4), ((this.getWidth()-200)/(2*POS))*(2*i+1)+100, this.getHeight()-80); 
+		for (int i = 0; i < pos; i++){	//draws X axis numbers
+			g.drawString(String.valueOf(i+dice), ((this.getWidth()-200)/(2*pos))*(2*i+1)+100, this.getHeight()-80); 
 		}
-		for (int i = 0; i < 4; i++){	//draws Y axis numbers
-			g.drawString(String.valueOf((i+1)*50), 70, ((this.getHeight()-200)/4)*(4-(i+1))+105);
+		for (int i = 0; i < (max/50); i++){	//draws Y axis numbers
+			g.drawString(String.valueOf((i+1)*50), 70, ((this.getHeight()-200)/(max/50))*((max/50)-(i+1))+105);
 		}
 		g.drawString(String.valueOf(0), 80, this.getHeight()-95);
 		repaint();
@@ -58,37 +67,52 @@ public class Display extends JFrame implements MouseListener{
 	
 	void refreshBars(Graphics g){
 		g.setColor(Color.BLACK);
-		for (int i = 0; i < POS; i++){
-			g.drawRect(((this.getWidth()-200)/POS)*i+100, (int)(this.getHeight()-h.get(""+(i+4))*y_scale-100), (this.getWidth()-200)/POS, (int)(h.get(""+(i+4))*y_scale));
+		for (int i = 0; i < pos; i++){
+			g.drawRect(((this.getWidth()-200)/pos)*i+100, (int)(this.getHeight()-h.get(""+(i+dice))*y_scale-100), (this.getWidth()-200)/pos, (int)(h.get(""+(i+dice))*y_scale));
 		}
 		g.setColor(Color.BLUE);
-		for (int i = 0; i < POS; i++){
-			g.fillRect(((this.getWidth()-200)/POS)*i+100+1, (int)(this.getHeight()-h.get(""+(i+4))*y_scale-100+1), (this.getWidth()-200)/POS-1, (int)(h.get(""+(i+4))*y_scale-1));
+		for (int i = 0; i < pos; i++){
+			g.fillRect(((this.getWidth()-200)/pos)*i+100+1, (int)(this.getHeight()-h.get(""+(i+dice))*y_scale-100+1), (this.getWidth()-200)/pos-1, (int)(h.get(""+(i+dice))*y_scale-1));
 		}
 	}
 	
-	public static void initRolls() {
-        Die roller1 = new Die(SIDES);
-        Die roller2 = new Die(SIDES);
-        Die roller3 = new Die(SIDES);
-        Die roller4 = new Die(SIDES);
-        String rollSum;
+	public void initrolls() {
+		int totalSum;
+       
+		for (int i = 0; i < dice; i++){
+			rollers[i] = new Die(sides);
+		}
+		
+        String rollsum;
         
-        for (int i = 0; i < 21; i++){
-        	h.put("" + (i+4), 0);
+        for (int i = 0; i < pos; i++){
+        	h.put("" + (i+dice), 0);
         }
         
-        for (int i = 0; i < 1000; i++) {
-            rollSum = "" + (roller1.roll() + roller2.roll() + roller3.roll() + roller4.roll());
-            h.put(rollSum, (Integer) (h.get(rollSum)) + 1);
+        for (int i = 0; i < rolls; i++) {
+        	totalSum = 0;
+        	for (int j = 0; j < dice; j++){
+    			totalSum = totalSum + rollers[j].roll();
+    		}
+            rollsum = "" + totalSum;
+            h.put(rollsum, (Integer) (h.get(rollsum)) + 1);
         }
     }
-
+	
+	public void findMax(){
+		for (int i = 0; i < pos; i++){
+			if (h.get(""+ (i+dice)) > max){
+				max = h.get("" + (i+dice));
+			}
+		}
+		max = max + 100 - (max % 100);
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		erase = true;
-		initRolls();
+		initrolls();
 	}
 
 	@Override
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
+import java.io.*;
+
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Display disp = new Display();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try{
+        System.out.print("Sides:");
+        int s = Integer.parseInt(br.readLine());
+        System.out.print("Dice:");
+        int d = Integer.parseInt(br.readLine());
+        Die[] rollers = new Die[d];
+        System.out.print("How many rolls?");
+        int r = Integer.parseInt(br.readLine());
+      
+		Display disp = new Display(r, s, rollers);
 		disp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		disp.setVisible(true);
+		}catch(NumberFormatException nfe){
+            System.err.println("That's not a number");
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
